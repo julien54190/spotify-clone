f2d6b2e6d0fd4bb84a6060dd02f17915874ae207
julien54190 <lucadello.julien@hotmail.fr>
2025-02-20 17:51:13 +0100

    add mapper interface song

---
+++ spotify-clone-back/src/main/java/fr/julien/spotify_clone_back/catalogcontext/application/mapper/SongContentMapper.java
@@ -0,0 +1,8 @@
+package fr.julien.spotify_clone_back.catalogcontext.application.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface SongContentMapper {
+
+}
