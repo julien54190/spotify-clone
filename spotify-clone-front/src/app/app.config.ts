16f6322da0d6fd3b3010c50dd4b2e11a9391c2bc
julien54190 <lucadello.julien@hotmail.fr>
2025-03-20 11:22:10 +0100

    add auth.service

--- spotify-clone-front/src/app/app.config.ts
+++ spotify-clone-front/src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+
+import {provideRouter} from '@angular/router';
 
 import { routes } from './app.routes';
+import { ApplicationConfig } from '@angular/core';
+import {provideHttpClient, withXsrfConfiguration} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withXsrfConfiguration({cookieName: 'XSRF-TOKEN', headerName: 'X-XSRF-TOKEN'})
+    )
+  ]
 };
